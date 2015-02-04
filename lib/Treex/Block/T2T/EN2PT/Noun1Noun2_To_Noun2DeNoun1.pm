03177ecf75c3ec808ed47306f1bc98403400450a
João Rodrigues <joao.rodrigues@di.fc.ul.pt>
2015-02-04 10:13:03 +0000

    Não move nouns entre conjunções ou coordenações

--- lib/Treex/Block/T2T/EN2PT/Noun1Noun2_To_Noun2DeNoun1.pm
+++ lib/Treex/Block/T2T/EN2PT/Noun1Noun2_To_Noun2DeNoun1.pm
@@ -13,6 +13,7 @@ sub process_ttree {
     foreach my $tnode ( $troot->get_descendants ) {
         my $parent = $tnode->get_parent;
         if (( $tnode->formeme || "" ) =~ /^n:(?:attr|de\+X)/ and
+                (( $parent->functor || "" ) !~ /^(CONJ|COORD)$/ ) and
                 (( $parent->formeme || "" ) =~ /^n:/ ) and
                 $tnode->precedes($parent)) {
             $tnode->shift_after_node($parent);
