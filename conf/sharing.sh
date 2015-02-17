0e265085396c69e0c45cef263ce68a46c2c3395a
Luis Gomes <luismsgomes@gmail.com>
2015-02-17 12:37:52 +0000

    fixed sharing configuration

--- conf/sharing.sh
+++ conf/sharing.sh
@@ -4,6 +4,6 @@ upload_ssh_host="nlx-server.di.fc.ul.pt"
 upload_ssh_port=22
 upload_ssh_path="public_html/qtleap/share"
 
-download_base_url="http://nlx-server.di.fc.ul.pt/~lgomes/qtleap/share"
-download_user="qtleap"
-download_password="paeltqtleap"
+download_http_base_url="http://nlx-server.di.fc.ul.pt/~lgomes/qtleap/share"
+download_http_user="qtleap"
+download_http_password="paeltqtleap"
