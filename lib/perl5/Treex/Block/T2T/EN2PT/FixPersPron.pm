cbd60e8090bf6c1739571aa82a2bf9e7023cc963
Luis Gomes <luismsgomes@gmail.com>
2015-02-25 15:52:49 +0000

    print more informative messages

--- lib/perl5/Treex/Block/T2T/EN2PT/FixPersPron.pm
+++ lib/perl5/Treex/Block/T2T/EN2PT/FixPersPron.pm
@@ -8,15 +8,16 @@ extends 'Treex::Core::Block';
 sub process_tnode {
 	my ( $self, $tnode ) = @_;
 
-
     my $src_tnode = $tnode->src_tnode();
 
     if ($src_tnode and $src_tnode->t_lemma eq "#PersPron" ) {
+            my $old_t_lemma = $tnode->t_lemma;
             $tnode->set_t_lemma("#PersPron");
-            print STDERR "FIXED\n";
-
             if ($tnode->gram_gender eq "neut") {
+                print STDERR "Treex::Block::T2T::EN2PT::FixPersPron: changed lemma from $old_t_lemma to #PersPron and gender from neut to anim\n";
                 $tnode->set_gram_gender("anim");
+            } else {
+                print STDERR "Treex::Block::T2T::EN2PT::FixPersPron: changed lemma from $old_t_lemma to #PersPron\n";
             }
     }
 
