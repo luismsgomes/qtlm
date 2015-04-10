7c64a1d251ba8a9753b1718ad134b13c1234054f
Luís Gomes <luismsgomes@gmail.com>
2015-04-10 17:53:08 +0100

    added missing dependencies

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,32 +1,33 @@
 #! /bin/bash
 
 if [ -z ${TMT_ROOT} ]; then
-	echo "$TMT_ROOT is not set." >&2
-	echo "Please set TMT_ROOT to the directory where you want tectomt to be checked out." >&2
-	echo "For example: TMT_ROOT=$HOME/code/tectomt" >&2
-	exit 1
+    echo "$TMT_ROOT is not set." >&2
+    echo "Please set TMT_ROOT to the directory where you want tectomt to be checked out." >&2
+    echo "For example: TMT_ROOT=$HOME/code/tectomt" >&2
+    exit 1
 fi
 
 if ! [ -d "$TMT_ROOT" ]; then
-	svn checkout --username public --password public \
-		"https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk" \
-		"$TMT_ROOT"
+    svn checkout --username public --password public \
+        "https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk" \
+        "$TMT_ROOT"
 fi
 
 sudo apt-get install --yes \
-	unattended-upgrades \
-	bash-completion \
-	build-essential \
-	git \
-	subversion \
-	gcc-4.8 \
-	g++-4.8 \
-	python-pip \
-	xorg-dev \
-	libxml2-dev \
-	zlib1g-dev \
-	software-properties-common \
-	python-software-properties # needer for add-apt-repository
+    unattended-upgrades \
+    bash-completion \
+    build-essential \
+    git \
+    subversion \
+    gcc-4.8 \
+    g++-4.8 \
+    python-pip \
+    python-dev \
+    xorg-dev \
+    libxml2-dev \
+    zlib1g-dev \
+    software-properties-common \
+    python-software-properties # needer for add-apt-repository
 
 sudo add-apt-repository --yes ppa:openjdk-r/ppa
 sudo apt-get update
@@ -38,27 +39,28 @@ sudo cpanm -n Tk
 sudo cpanm --installdeps Treex::Core
 # There are dependencies of other (non-Core) Treex modules
 sudo cpanm \
-	Ufal::MorphoDiTa \
-	Ufal::NameTag Lingua::Interset \
-	URI::Find::Schemeless \
-	PerlIO::gzip \
-	Text::Iconv \
-	Cache::Memcached \
-	Email::Find XML::Twig \
-	String::Util \
-	String::Diff \
-	List::Pairwise \
-	MooseX::Role::AttributeOverride \
-	YAML::Tiny \
-	Graph Tree::Trie \
-	Text::Brew \
-	App::Ack \
-	RPC::XML \
-	UUID::Generator::PurePerl \
-	App/whichpm.pm \
+    Ufal::MorphoDiTa \
+    Ufal::NameTag Lingua::Interset \
+    URI::Find::Schemeless \
+    PerlIO::gzip \
+    Text::Iconv \
+    Cache::Memcached \
+    Email::Find XML::Twig \
+    String::Util \
+    String::Diff \
+    List::Pairwise \
+    MooseX::Role::AttributeOverride \
+    YAML::Tiny \
+    Graph Tree::Trie \
+    Text::Brew \
+    App::Ack \
+    RPC::XML \
+    UUID::Generator::PurePerl \
+    File::HomeDir \
+    App/whichpm.pm
 
 sudo cpanm --force \
-	AI::MaxEntropy \
+    AI::MaxEntropy \
 
 sudo pip install scikit-learn numpy scipy
 
