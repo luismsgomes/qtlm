f9ac6be09e4b228c2617ac8d51ba121835ece560
Luís Gomes <luismsgomes@gmail.com>
2015-05-11 15:46:20 +0100

    fixed the sharing URLs

--- conf/sharing.sh
+++ conf/sharing.sh
@@ -2,8 +2,8 @@
 upload_ssh_user="qtleap-dev"
 upload_ssh_host="nlx-server.di.fc.ul.pt"
 upload_ssh_port=22
-upload_ssh_path="public_html/qtleap/share"
+upload_ssh_path="public_html"
 
-download_http_base_url="http://nlx-server.di.fc.ul.pt/~qtleap-dev/share"
+download_http_base_url="http://nlx-server.di.fc.ul.pt/~qtleap-dev"
 download_http_user="qtleap"
 download_http_password="paeltqtleap"
