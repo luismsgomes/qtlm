a502efcdad888094b3af72199b8488a405e212ab
Luís Gomes <luismsgomes@gmail.com>
2015-06-09 16:58:04 +0100

    minor fix

--- conf/testsets/en-pt/emnlp2015.sh
+++ conf/testsets/en-pt/emnlp2015.sh
@@ -1,4 +1,2 @@
-testset_files="
-	corpora/qtleap/v1.1/qtleap_2a_firsthalf.enpt.gz
-	corpora/qtleap/v1.1/qtleap_2q_firsthalf.enpt.gz
-"
+testset_files="corpora/qtleap/v1.1/qtleap_2a_firsthalf.enpt.gz
+			   corpora/qtleap/v1.1/qtleap_2q_firsthalf.enpt.gz"
