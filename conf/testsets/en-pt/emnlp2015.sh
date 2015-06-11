3a51560b29b592e03fff98307c6272cbb2aa2e5d
Luís Gomes <luismsgomes@gmail.com>
2015-06-11 11:12:56 +0100

    fix

--- conf/testsets/en-pt/emnlp2015.sh
+++ conf/testsets/en-pt/emnlp2015.sh
@@ -1,2 +1,1 @@
-testset_files="corpora/qtleap/v1.1/qtleap_2a_firsthalf.enpt.gz
-			   corpora/qtleap/v1.1/qtleap_2q_firsthalf.enpt.gz"
+testset_files="corpora/qtleap/v1.1/qtleap_2a_firsthalf.enpt.gz corpora/qtleap/v1.1/qtleap_2q_firsthalf.enpt.gz"
