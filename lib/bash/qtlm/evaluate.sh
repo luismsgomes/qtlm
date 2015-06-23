095546fdbe6134192f22cc38e0fd3298b94eed6f
Luís Gomes <luismsgomes@gmail.com>
2015-06-23 18:40:49 +0100

    evaluation dir now is specific to each QTLM_CONF

--- lib/bash/qtlm/evaluate.sh
+++ lib/bash/qtlm/evaluate.sh
@@ -6,21 +6,22 @@ function evaluate {
     local doing="evaluating ${src^^}->${trg^^} translation of testset $testset"
     log "$doing"
     load_testset_config
-    create_dir eval_$testset
+    eval_dir=eval_${lang_pair}_${dataset}_${train_date}_${testset}
+    create_dir $eval_dir
     local remote_path
     for remote_path in $testset_files; do
         local test_file=$(basename $remote_path .$lang1$lang2.gz)
-        local local_path=eval_$testset/$(basename $remote_path)
+        local local_path=$eval_dir/$(basename $remote_path)
         download_from_share $remote_path $local_path
-        if ! test -f eval_$testset/$test_file.$lang1.txt ||
-                ! test -f eval_$testset/$test_file.$lang2.txt; then
+        if ! test -f $eval_dir/$test_file.$lang1.txt ||
+                ! test -f $eval_dir/$test_file.$lang2.txt; then
             zcat $local_path |
             $QTLM_ROOT/tools/prune_unaligned_sentpairs.py |
-            tee >(cut -f 1 > eval_$testset/$test_file.$lang1.txt) |
-                  cut -f 2 > eval_$testset/$test_file.$lang2.txt
+            tee >(cut -f 1 > $eval_dir/$test_file.$lang1.txt) |
+                  cut -f 2 > $eval_dir/$test_file.$lang2.txt
         fi
         check_transfer_models
-        if test -f eval_$testset/$test_file.${src}2$trg.cache/.finaltouch; then
+        if test -f $eval_dir/$test_file.${src}2$trg.cache/.finaltouch; then
             translate_from_cache $testset $test_file
         else
             translate_from_scratch $testset $test_file
@@ -35,17 +36,17 @@ function evaluate {
 
 function translate_from_scratch {
     local testset=$1 test_file=$2
-    local doing="translating eval_$testset/$test_file from scratch"
+    local doing="translating $eval_dir/$test_file from scratch"
     log "$doing"
-    if test -d "eval_$testset/$test_file.${src}2${trg}.cache"; then
-        find "eval_$testset/$test_file.${src}2${trg}.cache" -type f -name "*.treex.gz" -delete
+    if test -d "$eval_dir/$test_file.${src}2${trg}.cache"; then
+        find "$eval_dir/$test_file.${src}2${trg}.cache" -type f -name "*.treex.gz" -delete
     else
-        create_dir "eval_$testset/$test_file.${src}2${trg}.cache"
+        create_dir "$eval_dir/$test_file.${src}2${trg}.cache"
     fi
-    if test -d "eval_$testset/$test_file.${src}2${trg}.final.new"; then
-        find "eval_$testset/$test_file.${src}2${trg}.final.new" -type f -name "*.treex.gz" -delete
+    if test -d "$eval_dir/$test_file.${src}2${trg}.final.new"; then
+        find "$eval_dir/$test_file.${src}2${trg}.final.new" -type f -name "*.treex.gz" -delete
     else
-        create_dir "eval_$testset/$test_file.${src}2${trg}.final.new"
+        create_dir "$eval_dir/$test_file.${src}2${trg}.final.new"
     fi
     $TMT_ROOT/treex/bin/treex --dump_scenario \
         Util::SetGlobal \
@@ -63,7 +64,7 @@ function translate_from_scratch {
             storable=0 \
             compress=1 \
             file_stem="" \
-            path=eval_$testset/$test_file.$src.final.new \
+            path=$eval_dir/$test_file.$src.final.new \
         Util::SetGlobal \
             language=$trg \
             selector=tst \
@@ -81,45 +82,45 @@ function translate_from_scratch {
         Write::Treex \
             storable=0 \
             compress=1 \
-            path=eval_$testset/$test_file.${src}2${trg}.cache \
+            path=$eval_dir/$test_file.${src}2${trg}.cache \
         "$QTLM_ROOT/scen/$lang1-$lang2/${trg}_t2w.scen" \
         Misc::JoinBundles \
         Write::Treex \
             storable=0 \
             compress=1 \
-            path=eval_$testset/$test_file.${src}2${trg}.final.new \
+            path=$eval_dir/$test_file.${src}2${trg}.final.new \
         Write::Sentences \
-	> eval_$testset/$test_file.${src}2${trg}.from_scratch.new.scen
+	> $eval_dir/$test_file.${src}2${trg}.from_scratch.new.scen
 
-    $TMT_ROOT/treex/bin/treex eval_$testset/$test_file.${src}2${trg}.from_scratch.new.scen \
-        < "eval_$testset/$test_file.$src.txt" 2> "eval_$testset/$test_file.${src}2${trg}.treexlog.new" |
-    postprocessing > "eval_$testset/$test_file.${trg}_mt.new.txt"
-    ls eval_$testset/$test_file.${src}2${trg}.cache |
+    $TMT_ROOT/treex/bin/treex $eval_dir/$test_file.${src}2${trg}.from_scratch.new.scen \
+        < "$eval_dir/$test_file.$src.txt" 2> "$eval_dir/$test_file.${src}2${trg}.treexlog.new" |
+    postprocessing > "$eval_dir/$test_file.${trg}_mt.new.txt"
+    ls $eval_dir/$test_file.${src}2${trg}.cache |
     sort --general-numeric-sort --key=1,1 --field-separator=. |
-    grep -P "\.treex.gz\$" > eval_$testset/$test_file.${src}2${trg}.cache/list.txt
-    touch eval_$testset/$test_file.${src}2${trg}.{cache,final.new}/.finaltouch
+    grep -P "\.treex.gz\$" > $eval_dir/$test_file.${src}2${trg}.cache/list.txt
+    touch $eval_dir/$test_file.${src}2${trg}.{cache,final.new}/.finaltouch
 
-    rotate_new_old eval_$testset/$test_file.${src}2${trg}.from_scratch scen
-    rotate_new_old eval_$testset/$test_file.${trg}_mt txt
-    rotate_new_old eval_$testset/$test_file.${src}2${trg}.treexlog
-    rotate_new_old eval_$testset/$test_file.${src}2${trg}.final
-    rotate_new_old eval_$testset/$test_file.$src.final
+    rotate_new_old $eval_dir/$test_file.${src}2${trg}.from_scratch scen
+    rotate_new_old $eval_dir/$test_file.${trg}_mt txt
+    rotate_new_old $eval_dir/$test_file.${src}2${trg}.treexlog
+    rotate_new_old $eval_dir/$test_file.${src}2${trg}.final
+    rotate_new_old $eval_dir/$test_file.$src.final
 
     log "finished $doing"
 }
 
 function translate_from_cache {
     local testset=$1 test_file=$2
-    local doing="translating eval_$testset/$test_file (using cached trees)"
+    local doing="translating $eval_dir/$test_file (using cached trees)"
     log "$doing"
-    if test -d "eval_$testset/$test_file.${src}2${trg}.final.new"; then
-        find "eval_$testset/$test_file.${src}2${trg}.final.new" -type f -name "*.treex.gz" -delete
+    if test -d "$eval_dir/$test_file.${src}2${trg}.final.new"; then
+        find "$eval_dir/$test_file.${src}2${trg}.final.new" -type f -name "*.treex.gz" -delete
     else
-        create_dir "eval_$testset/$test_file.${src}2${trg}.final.new"
+        create_dir "$eval_dir/$test_file.${src}2${trg}.final.new"
     fi
     $TMT_ROOT/treex/bin/treex --dump_scenario \
         Read::Treex \
-            from=@eval_$testset/$test_file.${src}2${trg}.cache/list.txt \
+            from=@$eval_dir/$test_file.${src}2${trg}.cache/list.txt \
         Util::SetGlobal \
             if_missing_bundles=ignore \
             language=$trg \
@@ -129,18 +130,18 @@ function translate_from_cache {
         Write::Treex \
             storable=0 \
             compress=1 \
-            path="eval_$testset/$test_file.${src}2${trg}.final.new" \
-        Write::Sentences > eval_$testset/$test_file.${src}2${trg}.from_cache.new.scen
+            path="$eval_dir/$test_file.${src}2${trg}.final.new" \
+        Write::Sentences > $eval_dir/$test_file.${src}2${trg}.from_cache.new.scen
 
-    $TMT_ROOT/treex/bin/treex eval_$testset/$test_file.${src}2${trg}.from_cache.new.scen \
-        2> "eval_$testset/$test_file.${src}2${trg}.treexlog.new" |
-    postprocessing > "eval_$testset/$test_file.${trg}_mt.new.txt"
-    touch eval_$testset/$test_file.${src}2${trg}.final.new/.finaltouch
+    $TMT_ROOT/treex/bin/treex $eval_dir/$test_file.${src}2${trg}.from_cache.new.scen \
+        2> "$eval_dir/$test_file.${src}2${trg}.treexlog.new" |
+    postprocessing > "$eval_dir/$test_file.${trg}_mt.new.txt"
+    touch $eval_dir/$test_file.${src}2${trg}.final.new/.finaltouch
 
-    rotate_new_old eval_$testset/$test_file.${src}2${trg}.from_cache scen
-    rotate_new_old eval_$testset/$test_file.${trg}_mt txt
-    rotate_new_old eval_$testset/$test_file.${src}2${trg}.treexlog
-    rotate_new_old eval_$testset/$test_file.${src}2${trg}.final
+    rotate_new_old $eval_dir/$test_file.${src}2${trg}.from_cache scen
+    rotate_new_old $eval_dir/$test_file.${trg}_mt txt
+    rotate_new_old $eval_dir/$test_file.${src}2${trg}.treexlog
+    rotate_new_old $eval_dir/$test_file.${src}2${trg}.final
 
     log "finished $doing"
 }
@@ -149,14 +150,14 @@ function check_num_lines {
     local testset=$1 test_file=$2
     # first let's check if translation went OK and we got as many lines at the
     # output as there are in the input
-    local num_lines_in=$(wc -l < "eval_$testset/$test_file.$src.txt")
+    local num_lines_in=$(wc -l < "$eval_dir/$test_file.$src.txt")
     local num_lines_out=0
-    if test -f "eval_$testset/$test_file.${trg}_mt.txt"; then
-        num_lines_out=$(wc -l < "eval_$testset/$test_file.${trg}_mt.txt")
+    if test -f "$eval_dir/$test_file.${trg}_mt.txt"; then
+        num_lines_out=$(wc -l < "$eval_dir/$test_file.${trg}_mt.txt")
     fi
     if ! test $num_lines_in -eq $num_lines_out; then
-        rm -f "eval_$testset/$test_file.${trg}_mt.txt"
-        fatal "translation failed; check Treex output at eval_$testset/$test_file.${src}2${trg}.treexlog"
+        rm -f "$eval_dir/$test_file.${trg}_mt.txt"
+        fatal "translation failed; check Treex output at $eval_dir/$test_file.${src}2${trg}.treexlog"
     fi
 }
 
@@ -164,29 +165,29 @@ function create_html_table {
     local testset=$1 test_file=$2
     # now let's create an HTML table showing the source, reference and machine
     #  translated sentences being evaluated
-    paste eval_$testset/$test_file.{$src,$trg,${trg}_mt}.txt |
+    paste $eval_dir/$test_file.{$src,$trg,${trg}_mt}.txt |
     $QTLM_ROOT/tools/tsv_to_html.py \
         "${testset} testset (${src^^}-${trg^^}) side by side with ${trg^^}/MT output ($dataset/$train_date)" \
         "$test_file.${src}2${trg}.final/{id1:03}.treex.gz" \
-        > eval_$testset/$test_file.${src}2${trg}.$src-$trg-mt_$trg.new.html
-    rotate_new_old eval_$testset/$test_file.${src}2${trg}.$src-$trg-mt_$trg html
+        > $eval_dir/$test_file.${src}2${trg}.$src-$trg-mt_$trg.new.html
+    rotate_new_old $eval_dir/$test_file.${src}2${trg}.$src-$trg-mt_$trg html
 }
 
 function create_ngram_summary {
     local testset=$1 test_file=$2
-    $QTLM_ROOT/tools/comparegrams.py eval_$testset/$test_file.{$trg,${trg}_mt}.txt \
-        > eval_$testset/$test_file.${src}2${trg}.new.ngrams
-    rotate_new_old eval_$testset/$test_file.${src}2${trg} ngrams
+    $QTLM_ROOT/tools/comparegrams.py $eval_dir/$test_file.{$trg,${trg}_mt}.txt \
+        > $eval_dir/$test_file.${src}2${trg}.new.ngrams
+    rotate_new_old $eval_dir/$test_file.${src}2${trg} ngrams
 }
 
 function run_mteval {
     local testset=$1 test_file=$2
-    if test -f eval_$testset/$test_file.${src}2${trg}.bleu && \
-            test eval_$testset/$test_file.${src}2${trg}.bleu -nt eval_$testset/$test_file.${trg}_mt.txt; then
+    if test -f $eval_dir/$test_file.${src}2${trg}.bleu && \
+            test $eval_dir/$test_file.${src}2${trg}.bleu -nt $eval_dir/$test_file.${trg}_mt.txt; then
         log "evaluation is up-to-date; skipping mteval"
         return
     fi
-    local doing="running mteval-v13a.pl on translation of eval_$testset/$test_file"
+    local doing="running mteval-v13a.pl on translation of $eval_dir/$test_file"
     log "$doing"
     local json="{\
         \"srclang\":\"$src\",\
@@ -195,18 +196,18 @@ function run_mteval {
         \"sysid\":\"qtleap:$QTLM_CONF\",\
         \"refid\":\"human\"}"
     $QTLM_ROOT/tools/txt_to_mteval_xml.py src "$json" "(.*)\.$src\.txt" \
-        eval_$testset/$test_file.$src.txt > eval_$testset/$test_file.${src}2${trg}.src.xml
+        $eval_dir/$test_file.$src.txt > $eval_dir/$test_file.${src}2${trg}.src.xml
     $QTLM_ROOT/tools/txt_to_mteval_xml.py ref "$json" "(.*)\.$trg\.txt" \
-        eval_$testset/$test_file.$trg.txt > eval_$testset/$test_file.${src}2${trg}.ref.xml
+        $eval_dir/$test_file.$trg.txt > $eval_dir/$test_file.${src}2${trg}.ref.xml
     $QTLM_ROOT/tools/txt_to_mteval_xml.py tst "$json" "(.*)\.${trg}_mt\.txt" \
-        eval_$testset/$test_file.${trg}_mt.txt > eval_$testset/$test_file.${src}2${trg}.tst.xml
+        $eval_dir/$test_file.${trg}_mt.txt > $eval_dir/$test_file.${src}2${trg}.tst.xml
     $QTLM_ROOT/tools/mteval-v13a.pl \
-            -s eval_$testset/$test_file.${src}2${trg}.src.xml \
-            -r eval_$testset/$test_file.${src}2${trg}.ref.xml \
-            -t eval_$testset/$test_file.${src}2${trg}.tst.xml \
-        > eval_$testset/$test_file.${src}2${trg}.bleu.new
-    rm -f eval_$testset/$test_file.${src}2${trg}.{src,ref,tst}.xml
-    rotate_new_old eval_$testset/$test_file.${src}2${trg}.bleu
+            -s $eval_dir/$test_file.${src}2${trg}.src.xml \
+            -r $eval_dir/$test_file.${src}2${trg}.ref.xml \
+            -t $eval_dir/$test_file.${src}2${trg}.tst.xml \
+        > $eval_dir/$test_file.${src}2${trg}.bleu.new
+    rm -f $eval_dir/$test_file.${src}2${trg}.{src,ref,tst}.xml
+    rotate_new_old $eval_dir/$test_file.${src}2${trg}.bleu
     log "finished $doing"
 }
 
@@ -220,16 +221,16 @@ function load_testset_config {
         fatal "please fix $testset_config_file"
     fi
     # let's check if testset_files contains a proper list of files
-    local num_files=$(wc -w <<< $testset_files)
-    local unique_basenames=$(map basename $testset_files | sort -u)
-    local num_unique_basenames=$(wc -w <<< $unique_basenames)
-    if test $num_files -ne $num_unique_basenames; then
-        fatal "check testset configuration \"$testset\": some files have the same basename"
-    fi
-    local num_matched=$(grep -cP "\.$lang1$lang2\.gz\$" <<< $unique_basenames)
-    if test $num_matched -ne $num_unique_basenames; then
-        fatal "check testset configuration \"$testset\": some files don't have $lang1$lang2.gz suffix"
-    fi
+    #local num_files=$(wc -w <<< $testset_files)
+    #local unique_basenames=$(map basename $testset_files | sort -u)
+    #local num_unique_basenames=$(wc -w <<< $unique_basenames)
+    #if test $num_files -ne $num_unique_basenames; then
+    #    fatal "check testset configuration \"$testset\": some files have the same basename"
+    #fi
+    #local num_matched=$(grep -cP "\.$lang1$lang2\.gz\$" <<< $unique_basenames)
+    #if test $num_matched -ne $num_unique_basenames; then
+    #    fatal "check testset configuration \"$testset\": some files don't have $lang1$lang2.gz suffix"
+    #fi
 }
 
 function list_all_testsets {
@@ -294,14 +295,14 @@ function clean {
     load_testset_config
     for remote_path in $testset_files; do
         local test_file=$(basename $remote_path .$lang1$lang2.gz)
-        if test -d eval_$testset/$test_file.${src}2${trg}.cache; then
-            rm -rvf eval_$testset/$test_file.${src}2${trg}.cache
+        if test -d $eval_dir/$test_file.${src}2${trg}.cache; then
+            rm -rvf $eval_dir/$test_file.${src}2${trg}.cache
         fi
-        if test -d eval_$testset/$test_file.${src}2${trg}.final; then
-            rm -rvf eval_$testset/$test_file.${src}2${trg}.final
+        if test -d $eval_dir/$test_file.${src}2${trg}.final; then
+            rm -rvf $eval_dir/$test_file.${src}2${trg}.final
         fi
-        if test -d eval_$testset/$test_file.${src}.final; then
-            rm -rvf eval_$testset/$test_file.${src}.final
+        if test -d $eval_dir/$test_file.${src}.final; then
+            rm -rvf $eval_dir/$test_file.${src}.final
         fi
     done
     log "finished $doing"
